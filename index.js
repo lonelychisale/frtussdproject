52f8bb1f3f27a65c28586fcdf4769d384a639c36
lonelychisale <cis-003-18@must.ac.mw>
2023-03-30 00:47:35 -0700

    expecteds

--- index.js
+++ index.js
@@ -279,6 +279,8 @@ else if(dataarray[1]=='2' && dataarray[3]=='1' && dataarraysize==4){
 }
 
 else if(dataarray[1]=='2' && dataarray[3]=='2' && dataarray[4]!='' && dataarraysize==5){
+  districtindex = `${--dataarray[2]}`
+  districtname = weatherdistricts[districtindex].name
   districtexpectionsarray = []
   districtexpecteds = weatherdistricts[districtindex].expected
   
