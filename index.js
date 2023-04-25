41f839c489739ea3e0480d0f612e60571a0a7198
lonelychisale <cis-003-18@must.ac.mw>
2023-04-25 07:57:46 -0700

    dffrr

--- index.js
+++ index.js
@@ -72,6 +72,8 @@ const ref = db.ref("weather/districts");
 phone ='0993347204'
 
 
+var num = '10'
+console.log(`${num}`-10)
 
 //.............................declaring global advisory variables for english..................................
 namesss = advesoryjson.sectors[0].categories[0].products
@@ -626,7 +628,7 @@ else if(dataarraysize==11 && dataarray[0]=='1' && dataarray[1]=='1' && dataarray
 }
   
 else if (dataarraysize == 12 && dataarray[0] == "1" && dataarray[2]=='0' &&  dataarray[11]!='' && language =="English") {
-  secregdstrictindex          =`${dataarray[4]-10}`
+  secregdstrictindex          =`${dataarray[4]}`-10
   secregvarietyindex          =`${--dataarray[6]}`
   secregenderindex            =`${--dataarray[8]}`
   secregageindex              =`${--dataarray[9]}`
@@ -651,7 +653,7 @@ else if (dataarraysize == 12 && dataarray[0] == "1" && dataarray[2]=='0' &&  dat
 
   
 
-/*
+
   newregref.child(phoneNumber).set({
     userdistrict:nextRegistratindistrict   ,
     userTA:RegistrationTA,
@@ -665,7 +667,7 @@ else if (dataarraysize == 12 && dataarray[0] == "1" && dataarray[2]=='0' &&  dat
     userPhonenumber:phoneNumber
 
   })
-  */
+  
   
 
 
